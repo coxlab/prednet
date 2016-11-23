01004c12fe95fee4f8baa18d5c60dc66b6d9a4af
EderSantana <edercsjr@gmail.com>
2016-11-23 18:26:10 -0500

    new backend dim ordering

--- kitti_evaluate.py
+++ kitti_evaluate.py
@@ -18,7 +18,10 @@ from keras.layers import Input, Dense, Flatten
 from prednet import PredNet
 from data_utils import SequenceGenerator
 from kitti_settings import *
-
+try:  # new keras uses Tensorflow as default backend
+    K.set_image_dim_ordering('th')
+except:
+    pass
 
 n_plot = 40
 batch_size = 10

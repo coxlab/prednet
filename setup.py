a44113d4d8140458c5738ef2382b4b5b0d13a439
Srolsorama <Stein.robert.olsen@gmail.com>
2019-07-10 10:53:28 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setuptools.setup(
     name='prednet',
     version='0.1',
     packages=setuptools.find_packages(),
-    python_requires='==3.6.*',
+    python_requires='>=3.5.*',
     install_requires=[
         'tensorflow-gpu==1.13.1',
         'Keras==2.2.4',

3b1662df5a7ef9d2527d81952ecc9435774a1a38
David A. Hannasch <David.A.Hannasch@gmail.com>
2019-09-23 13:43:57 -0600

    use kitti_hkl

--- tests/kitti_settings.py
+++ tests/kitti_settings.py
@@ -1,6 +1,6 @@
 # Where KITTI data will be saved if you run process_kitti.py
 # If you directly download the processed data, change to the path of the data.
-DATA_DIR = './kitti_data/'
+DATA_DIR = './kitti_hkl/'
 
 # Where model weights and config will be saved if you run kitti_train.py
 # If you directly download the trained weights, change to appropriate path.

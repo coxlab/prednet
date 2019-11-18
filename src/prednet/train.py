3a83f91de916fa768c8ef2f4254f7345e137b6b5
David A. Hannasch <David.A.Hannasch@gmail.com>
2019-11-18 14:59:46 -0700

    print

--- src/prednet/train.py
+++ src/prednet/train.py
@@ -41,6 +41,7 @@ def train_on_single_video(path_to_video,
   source_list = [path_to_video for frame in array]
   assert len(source_list) == array.shape[0]
   numberOfFrames = array.shape[0]
+  print('train_on_single_video about to call train_on_arrays_and_sources')
   return train_on_arrays_and_sources(array[:numberOfFrames//2], source_list[:numberOfFrames//2],
                                      array[numberOfFrames//2:], source_list[numberOfFrames//2:],
                                      path_to_save_model_json=path_to_save_model_json,

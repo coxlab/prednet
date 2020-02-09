0dd27c28261d98a179d9dda167a854a8175f6d46
David A. Hannasch <David.A.Hannasch@gmail.com>
2020-02-09 10:11:06 -0700

    add jupyter to CLI

--- src/prednet/cli.py
+++ src/prednet/cli.py
@@ -15,30 +15,45 @@ Why does this file exist, and why not put this in __main__?
   Also see (1) from http://click.pocoo.org/5/setuptools/#setuptools-integration
 """
 import argparse
+import os.path
+import pkg_resources
+
 import jupyterlab.labapp
-import prednet.train
-import prednet.evaluate
 
 parser = argparse.ArgumentParser(description='Train and evaluate on single video.')
-parser.add_argument('paths_to_videos', nargs='+', help="Path to source video file.")
+# parser.add_argument('train_or_predict', nargs='?', choices=('train', 'predict', 'jupyter'), help="Only train or only evaluate.")
+# There appears to be no native way in argparse to have an optional positional argument prior to a list of positional arguments.
 parser.add_argument('--model-file', help="Path to load and/or save the model file.")
 parser.add_argument('--subsequence-length', type=int,
                     help="Length of subsequences (at the end of each subsequence, PredNet forgets its state and starts fresh).")
 parser.add_argument('--number-of-epochs', type=int, default=150, help="Number of epochs to use in training.")
 parser.add_argument('--steps-per-epoch', type=int, default=125, help="Steps per epoch to use in training.")
-parser.add_argument('train_or_predict', nargs='?', choices=('train', 'predict', 'jupyter'), help="Only train or only evaluate.")
+subparsers = parser.add_subparsers(help='We can only train, or predict, or simply open a Jupyter instance.', dest='subparser_name')
+trainParser = subparsers.add_parser('train', help='Train a model.')
+predictParser = subparsers.add_parser('predict', help='Predict frames using a model (train if necessary).')
+jupyterParser = subparsers.add_parser('jupyter', help='Launch a Jupyter instance showing how you can use PredNet programmatically.')
+trainParser.add_argument('paths_to_videos', nargs='+', help="Paths to source video files.")
+predictParser.add_argument('paths_to_videos', nargs='+', help="Paths to source video files.")
 
 
 def main(args=None):
     args = parser.parse_args(args=args)
-    if len(args.paths_to_videos) != 1 and not args.model_file:
+    if args.subparser_name == 'jupyter': assert 'paths_to_videos' not in vars(args)
+    if 'paths_to_videos' in vars(args) and len(args.paths_to_videos) != 1 and not args.model_file:
       parser.error('If you want to specify more than one video, we cannot infer the name you want for the model file.')
-    if args.train_or_predict:
-      if args.train_or_predict == 'train':
+    if args.subparser_name == 'jupyter':
+      # open an included notebook showing how to do something simple with prednet
+      jupyterlab.labapp.main([pkg_resources.resource_filename(__name__, os.path.join('resources', 'quickstart.ipynb'))])
+    else:
+      # We put off importing prednet because there's a delay when the TensorFlow backend is loaded.
+      # The TensorFlow backend will be loaded as soon as we import either, so we might as well import both.
+      import prednet.train
+      import prednet.evaluate
+      if args.subparser_name == 'train':
           prednet.train.train_on_video_list(args.paths_to_videos,
                                             number_of_epochs=args.number_of_epochs,
                                             steps_per_epoch=args.steps_per_epoch)
-      elif args.train_or_predict == 'predict':
+      elif args.subparser_name == 'predict':
         # This might not be the best way to do things, but this function will
         # automatically use the saved model if the file(s) already exist.
         prednet.evaluate.save_predicted_frames_for_video_list(
@@ -47,12 +62,4 @@ def main(args=None):
             model_file_path=args.model_file,
             nt=args.subsequence_length,
             )
-      elif args.train_or_predict == 'jupyter':
-        jupyterlab.labapp.main()  # open an included notebook showing how to do something simple with prednet
-    else:
-        prednet.evaluate.save_predicted_frames_for_video_list(
-            args.paths_to_videos,
-            number_of_epochs=args.number_of_epochs, steps_per_epoch=args.steps_per_epoch,
-            model_file_path=args.model_file,
-            nt=args.subsequence_length,
-            )
+
